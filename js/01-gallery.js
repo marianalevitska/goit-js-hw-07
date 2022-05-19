4d53c975967a2cce5fc053fbccaf556f4f932de2
Mariana <mariamna5204@gmail.com>
2022-05-20 00:15:10 +0300

    Update 01-gallery.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -7,6 +7,8 @@ const galleryMarkup = createGalleryItem(galleryItems);
 galleryContainer.insertAdjacentHTML('beforeend', galleryMarkup);
 
 galleryContainer.addEventListener('click', onImageItemClick);
+galleryContainer.addEventListener('click', galleryClick);
+let modalWindow;
 
 function createGalleryItem(galleryItems) {
 
@@ -24,17 +26,18 @@ function createGalleryItem(galleryItems) {
     </div> `;
     }).join('');
 }
-function onImageItemClick() {
-    if (item.target.nodeName !== "IMG") {
+function onImageItemClick(e) {
+    if (e.target.nodeName !== "IMG") {
         return
     }
-    item.preventDefault();
-    const lageImage = evt.target.dataset.source;
+    e.preventDefault();
+    const lageImage = e.target.dataset.source;
     const instance = basicLightbox.create(`
     <img
-    src ="${evt.target.dataset.source}">`,
+    src ="${e.target.dataset.source}">`,
+
         {
-            onShow: (instance) => {
+            onShow: () => {
                 const listener = function (evt) {
                     if (evt.key === "Escape") {
                         document.removeEventListener('keydown', listener)
@@ -47,49 +50,5 @@ function onImageItemClick() {
     )
     instance.show()
 };
-
-
-
-
-// const pics = document.querySelector(".gallery");
-// let modalWindow;
-
-// const galleryClick = (evt) => {
-//     evt.preventDefault();
-
-//     const scrImg = evt.target.getAttribute("data-source");
-//     if (scrImg) {
-//         modalWindow.basicLightbox.create(`<img id="preview" scr="${scrImg}">`, {
-//             onShow: () => pics.addEventListener('keydown', handleToggler),
-//             onClose: () => pics.addEventListener('keydown', handleToggler),
-//         });
-//         modalWindow.show();
-//     }
-// };
-// const handleToggler = (evt) => {
-//     if (evt.key === 'Escape') {
-//         modalWindow.close()
-//     };
-// };
-// const newGallery = () => {
-//     const imgHTML = galleryItems.map(
-//         ({ original, preview, description }) => `
-//         <li class="gallery_item">
-//         <a class="gallery_link" href="${original}">
-//         <img
-//         class="gallery_img"
-//         scr="${preview}"
-//         alt="${description}"
-//         data-source="${original}"
-//         >
-//         </a>
-//         </li>`
-//     )
-//         .join("");
-//     pics.innerHTML = imgHTML;
-// };
-// newGallery();
-// pics.addEventListener("click", handleClick);
-
 
 console.log(galleryItems);
