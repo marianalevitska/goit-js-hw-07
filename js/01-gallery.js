c1232580f72327925f8f1a07b6a9dc82bf16facf
Mariana <mariamna5204@gmail.com>
2022-05-19 23:34:11 +0300

    Update 01-gallery.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,40 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const pics = document.querySelector(".gallery");
+let modalWindow;
 
-console.log(galleryItems);
+const galleryClick = (evt) => {
+    evt.preventDefault();
+
+    const scrImg = evt.target.getAttribute("data-source");
+    if (scrImg) {
+        modalWindow.basicLightbox.create(`<img id="preview" scr="${scrimg}">`, {
+            onShow: () => pics.addEventListener("keydown", handleToggler),
+            onClose: () => pics.addEventListener("keydown", handleToggler),
+        });
+        modalWindow.show();
+    }
+};
+const handleToggler = (evt) => {
+    if (evt.key === "Escape") modalWindow.close();
+};
+const newGallery = () => {
+    const imgHTML = galleryItems.map(
+        ({ original, preview, description }) => `
+        <li class="gallery_item">
+        <a class="gallery_link" href="${original}">
+        <img
+        class="gallery_img"
+        scr="${preview}"
+        alt="${description}"
+        data-source="${original}"
+        >
+        </a>
+        </li>`
+    )
+        .join("");
+    pics.innerHTML = imgHTML;
+};
+newGallery();
+pics.addEventListener("click", handleClick);
+// console.log(galleryItems);
